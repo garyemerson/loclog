7a2f45733684b875306fa99fd6b167e8440d7898
garyemerson <gmohammadioun@gmail.com>
2020-01-02 22:32:11 -0800

    update

--- loclog/AppDelegate.swift
+++ loclog/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var locDelegate = LocationDelegate()
     var requests: [Date] = []
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         LogEntry.log(
             msg: "app launched with options: " + (launchOptions?.map({opt in opt.key.rawValue}).joined(separator: ",") ?? "<none>"),
